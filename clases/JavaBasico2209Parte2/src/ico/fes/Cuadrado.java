e12c4b7195f227072345b3b7bb8ea2f3cfceec1f
YisusBRK8 <jesusgarcia114@aragon.unam.mx>
2022-04-28 12:55:52 -0500

    Commit de la clases de excepciones y un poco de metodos abstractos

---
+++ clases/JavaBasico2209Parte2/src/ico/fes/Cuadrado.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes;
+
+/**
+ *
+ * @author jesgm
+ */
+public class Cuadrado implements Figura{
+
+    @Override
+    public float calcularArea() {
+        System.out.println("Multiplicador lado por lado");
+        return 1.0f;
+    }
+    
+}
